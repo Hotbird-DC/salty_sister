2299c8f03b27b226b8baee7d448978fd255ad463
Phil Dugliss <phillipdugliss@gmail.com>
2019-11-19 15:15:20 -0600

    Home Page Style Update
    -Sections Editied
    -Background Color added
    -Font Color changed
    -Google fonts added

--- salty-sister/src/Componets/Grid_Home/index.js
+++ salty-sister/src/Componets/Grid_Home/index.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import RecipeCard from '../Recipe_Card_Home';
+import './style.css';
+
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -22,15 +24,20 @@ export default function FullWidthGrid() {
         <div className={classes.root}>
             <Grid container spacing={3}>
                 <Grid item xs={12}>
-                <img src= {require ('/Users/phildugliss/GitHub/salty_sister/salty-sister/src/Assets/Images/Oysters2.jpg')} style={{width: 980, height: 653}}  />
+                <h1>Salty Sister</h1>
+                <img src= {require ('/Users/phildugliss/GitHub/salty_sister/salty-sister/src/Assets/Images/Oysters2.jpg')} style={{width: 1270, height: 753}} alt= 'background' class='responsive'/>
                     
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <Paper className={classes.paper}>xs=12 sm=6
+                    
+                    <Paper className={classes.paper}>
+                    <h2>Current Recipes</h2>
                     </Paper>
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <Paper className={classes.paper}>xs=12 sm=6</Paper>
+                    <Paper className={classes.paper}>
+                        <h2>About Us</h2>
+                    </Paper>
                 </Grid>
                 <Grid item xs={6} sm={3}>
                     <RecipeCard></RecipeCard>
